29c66986bf41015da07d46a376a287b88363c097
reysheypua <rpua@byupathway.edu>
2025-11-07 09:54:51 +0800

    W02 Learning Activity: Abstraction

--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -8,5 +8,9 @@ public class Resume()
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
         
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDisplay();
+        }
     }
 }
