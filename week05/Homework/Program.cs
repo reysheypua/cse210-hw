d74b603baa303e6426d111671c219e54ee5e345c
reysheypua <rpua@byupathway.edu>
2025-11-28 14:49:12 +0800

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(assignment.GetSummary());
+
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
