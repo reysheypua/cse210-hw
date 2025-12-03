a807bc3ca6af75b9bb7ca40c650195304137f762
reysheypua <rpua@byupathway.edu>
2025-12-03 16:49:49 +0800

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Red", 3);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Green", 8.5, 5);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Yellow", 3.5);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
